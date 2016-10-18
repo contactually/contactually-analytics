7128980f308d300c3a64d9e6fa434f1721b32be7
Drew Banin <drew@8tracks.com>
2016-10-18 10:18:33 -0400

    fix cogs

--- models/finance/cogs.sql
+++ models/finance/cogs.sql
@@ -1,11 +1,8 @@
 
-with cogs_accounts as (
+with cogs_classes as (
 
-    select *
-    from {{ ref('quickbooks_accounts') }}
-        where classification = 'Expense'
-        and "type" = 'Cost of Goods Sold'
-        and active = TRUE
+    select * from {{ ref('quickbooks_classes') }}
+    where id in ('3100000000000547352', '3100000000000547361', '3100000000000559382')
 
 ),
 
@@ -13,16 +10,13 @@ ledger as (
 
     select * from {{ ref('quickbooks_general_ledger') }}
 
-),
-
-classes as (
-
-    select * from {{ ref('quickbooks_classes') }}
-
 )
 
-select l.txn_date as "date", c.id, c.name, l.amount, classes.name as class_name
-from cogs_accounts c
-join ledger l on c.id = l.account_id
-join classes on classes.id = l.class_id
-where l.transaction_type = 'debit'
+
+select
+    ledger.txn_date,
+    ledger.amount
+    cogs_classes.fully_qualified_name,
+from ledger
+join cogs_classes on ledger.class_id = cogs_classes.id
+where ledger.transaction_type = 'debit';
