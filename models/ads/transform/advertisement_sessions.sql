029fa73bbefd39c0125b544e8ead3784a537f0b9
Drew Banin <drew@8tracks.com>
2016-10-31 13:47:56 -0400

    create sessions <--> ads join table

--- models/ads/transform/advertisement_sessions.sql
+++ models/ads/transform/advertisement_sessions.sql
@@ -6,15 +6,42 @@
   )
 }}
 
+
+-- coalesce null values to empty strings so that our join on utm params
+-- can be a simple equality. Using (ad.utm_* = session.utm_* OR ad.utm_* is null) 
+-- results in an ugly nested-loop join and kills performance
+
 with sessions as (
 
-    select * from {{ ref('sessions_with_attribution') }}
+    select session_id,
+        session_start_tstamp::date as date,
+        coalesce(cleaned_source, '')   as cleaned_source,
+        coalesce(cleaned_medium, '')   as cleaned_medium,
+        coalesce(cleaned_campaign, '') as cleaned_campaign,
+        coalesce(cleaned_term, '')     as cleaned_term,
+        coalesce(cleaned_content, '')  as cleaned_content
+
+    from {{ ref('sessions_with_attribution') }}
 
 ),
 
 ads as (
 
-    select * from {{ ref('ad_performance_all') }}
+    select id,
+        date::date as date,
+        coalesce(utm_source, '')   as utm_source,
+        coalesce(utm_medium, '')   as utm_medium,
+        coalesce(utm_campaign, '') as utm_campaign,
+        coalesce(utm_term, '')     as utm_term,
+        coalesce(utm_content, '')  as utm_content
+
+    from {{ ref('ad_performance_all') }}
+    where
+        utm_source is not null and
+        utm_medium is not null and
+        utm_campaign is not null and
+        utm_term is not null and
+        utm_content is not null
 
 ),
 
@@ -22,25 +49,17 @@ ads as (
 with_ad_id as (
     select
         ads.id as ad_id,
-        s.session_id,
-        row_number() over (partition by s.session_id) as row_number
+        s.session_id
     from sessions as s
     inner join ads on
-        --(s.landing_page = ads.base_url OR ads_rollup.base_url is null) AND
-        (ads.utm_source   ilike s.cleaned_source   or ads.utm_source   is null) AND
-        (ads.utm_medium   ilike s.cleaned_medium   or ads.utm_medium   is null) AND
-        (ads.utm_campaign ilike s.cleaned_campaign or ads.utm_campaign is null) AND
-        (ads.utm_term     ilike s.cleaned_term     or ads.utm_term     is null) AND
-        (ads.utm_content  ilike s.cleaned_content  or ads.utm_content  is null) AND
-        NOT (
-          ads.utm_medium   is null AND
-          ads.utm_source   is null AND
-          ads.utm_campaign is null AND
-          ads.utm_term     is null AND
-          ads.utm_content  is null
-        )
+        ads.utm_source   = s.cleaned_source   and
+        ads.utm_medium   = s.cleaned_medium   and
+        ads.utm_campaign = s.cleaned_campaign and
+        ads.utm_term     = s.cleaned_term     and
+        ads.utm_content  = s.cleaned_content  and
+        ads.date         = s.date
+        -- TODO : check for landing page = Ad url?
 )
 
 
 select ad_id, session_id from with_ad_id
-where row_number = 1
