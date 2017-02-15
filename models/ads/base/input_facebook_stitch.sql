605223dbc22f65b07e7faa8be18687f144fd136e
Drew Banin <drew@fishtownanalytics.com>
2017-02-15 12:41:42 -0500

    remove extra logic

--- models/ads/base/input_facebook_stitch.sql
+++ models/ads/base/input_facebook_stitch.sql
@@ -2,8 +2,5 @@
 
 select
     creative.*,
-    creative_child.call_to_action__value__link as link_url
-
+    null::varchar as link_url
 from facebook_contactually_ads.facebook_adcreative_26288427 as creative
-left outer join facebook_contactually_ads.facebook_adcreative_26288427__object_story_spec__link_data__child_attachments as creative_child
-    on creative_child._sdc_source_key_id = creative.id
