a22ece9a7bfe3062a170126fba7cc569cbcf39de
Andrew Garrett <garrett528@gmail.com>
2017-07-02 13:07:13 -0400

    Fixed quickbooks_invoices

---
+++ models/finance/base/quickbooks_invoices_stitched.sql
@@ -0,0 +1,14 @@
+select
+    id::int,
+    totalamt as total_amt,
+    txndate::date as txn_date,
+    duedate::date as due_date,
+    balance,
+    null as delivery_type,
+    null as delivery_time,
+    emailstatus as email_status,
+    docnumber as doc_number,
+    customerref__value::int as customer_id,
+    metadata__createtime as created_at,
+    metadata__lastupdatedtime as updated_at
+from quickbooks.quickbooks_invoices
