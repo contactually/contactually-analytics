ecc2ceb4ed44812656a84d8d5c4cf2d6ce24e17c
Andrew Garrett <andrew.garrett@contactually.com>
2017-07-02 22:20:25 -0400

    Fixed issue with quickbooks_general_ledger

--- models/finance/base/quickbooks_invoices_stitched.sql
+++ models/finance/base/quickbooks_invoices_stitched.sql
@@ -2,13 +2,13 @@ select
     id::int,
     totalamt,
     txndate::date as txndate,
-    duedate::date as due_date,
+    duedate::date as duedate,
     balance,
     null as delivery_type,
     null as delivery_time,
-    emailstatus as email_status,
-    docnumber as doc_number,
-    customerref__value::int as customer_id,
-    metadata__createtime as created_at,
-    metadata__lastupdatedtime as updated_at
+    emailstatus as emailstatus,
+    docnumber as docnumber,
+    customerref__value::int as customerref__value,
+    metadata__createtime as metadata__createtime,
+    metadata__lastupdatedtime as metadata__lastupdatedtime
 from quickbooks.quickbooks_invoices
